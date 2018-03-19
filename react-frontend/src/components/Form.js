8c24c92d38dbbae0aec545da6c8af893cc01aeca
Michello Lo <michelle.erica.lo@gmail.com>
2018-03-19 12:42:40 +1100

    Added modal and form component

---
+++ react-frontend/src/components/Form.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export default class Form extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      quote: '',
+      author_id: '',
+
+
+
+    }
+
+    this._handleChange = this._handleChange.bind(this);
+    this._submitForm = this._submitForm.bind(this);
+
+  }
+
+  _handleChange(e){
+    this.setState({[e.target.name]: e.target.value});
+    console.log(this.state.quote);
+  }
+
+  _submitForm(e){
+    e.preventDefault();
+
+    axios.post('/quotes', {
+      contents: this.state.quote,
+      author_id: this.state.author_id
+    })
+    .then( response => {
+      console.log(response.data)
+    })
+    .catch(err => console.log(err))
+
+  }
+
+
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={this._submitForm}>
+          <h2> Quote </h2>
+          <textarea name="quote" onChange={ this._handleChange }></textarea>
+          <h2> Author </h2>
+          <input type="text" name="author_id" onChange={ this._handleChange }></input>
+          <br/>
+          <input type="submit" value="Submit"/>
+      </form>
+
+      </div>
+    )
+  }
+
+}
--- react-frontend/src/components/HomePage.js
+++ react-frontend/src/components/HomePage.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
+import ReactModal from 'react-modal'
 import Quotes from './Quotes'
+import Form from './Form'
 
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 export default class HomePage extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      showModal: false
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal(){
+    this.setState({showModal: true});
+  }
+
+  handleCloseModal(){
+    this.setState({showModal: false});
+  }
 
   render(){
 
@@ -13,7 +43,14 @@ export default class HomePage extends Component{
         <h1> Quote </h1>
 
           <div ><Quotes/></div>
-
+          <button onClick={this.handleOpenModal}>Trigger Modal</button>
+        <ReactModal
+           isOpen={this.state.showModal}
+           onRequestClose={this.handleCloseModal}
+           style={customStyles}
+        >
+          <Form/>
+        </ReactModal>
       </div>
     )
 
